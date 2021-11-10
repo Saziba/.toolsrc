c1073358eebd72cf0308e9d1a3e27bf49f6bd78d
Saziba <saziba@icloud.com>
2021-11-10 16:37:41 -0300

    Adding lots of aliases and creating the private aliases folder for aliases You don't want to push to the repo

--- aliasrc.sh
+++ aliasrc.sh
@@ -1,14 +1,18 @@
 
-ALIAS_PATH=$(dirname $0)/aliases
+load_aliases_from_path() {
+  ALIAS_PATH=$1
+  for a in $( ls $ALIAS_PATH 2>/dev/null )
+  do
+    type -a $a >/dev/null
+    ret=$?
+    if [ $ret -eq 1 ]
+    then
+      alias $a="$(cat $ALIAS_PATH/$a)"
+    else
+      echo "cannot create $a because $(type -a $a) "
+    fi
+  done
+}
 
-for a in $( ls $ALIAS_PATH )
-do
-  type -a $a >/dev/null
-  ret=$?
-  if [ $ret -eq 1 ]
-  then
-    alias $a="$(cat $ALIAS_PATH/$a)"
-  else
-    echo "cannot create $a because $(type -a $a) "
-  fi
-done
+load_aliases_from_path $(dirname $0)/aliases
+load_aliases_from_path $(dirname $0)/private_aliases
--- rc.sh
+++ rc.sh
@@ -1,2 +1,12 @@
-aliasrc=$(dirname $0)/aliasrc.sh
-[[ -s "$aliasrc" ]] && source $aliasrc
+
+where_am_i=$(dirname $0)
+
+run_if_exists(){
+    script=${where_am_i}/${1}.sh
+    [[ -s "$script" ]] && source $script
+}
+
+run_if_exists exportrc
+run_if_exists aliasrc
+run_if_exists otherrc
+
