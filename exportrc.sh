d8cbcfb2c6b49350a56e3beb9c44becada49b1d0
Itagyba Abondanza <saziba@icloud.com>
2022-10-20 15:52:35 -0300

    adding terraform alias

--- exportrc.sh
+++ exportrc.sh
@@ -1,1 +1,13 @@
-export DURATION="43200"
+#export DURATION="43200"
+
+
+load_vars_from_path() {
+  ALIAS_PATH=$1
+  for a in $( ls $ALIAS_PATH 2>/dev/null )
+  do
+    source $ALIAS_PATH/$a
+  done
+}
+
+load_vars_from_path $(dirname $0)/vars
+load_vars_from_path $(dirname $0)/private_vars
